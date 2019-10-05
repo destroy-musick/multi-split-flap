81effebca7b9db684da4c3b1d5463facd73c2469
David Webb <30623954+destroy-musick@users.noreply.github.com>
2019-10-05 12:54:57 +0100

    Allow for 3 different modes of operation
    including a change to the readme file, and included an example script

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # multi-split-flap
-Branch from Split Flap project, including original arduino code and modified wrapper script
+Branch from Split Flap project, including original arduino code and modified wrapper script. This is designed to help facilitate the use of multiple arduino mc's to act as one large unit
+
+<h1>**Usage**</h1>
+
+Before running the main.py script to initialise the split-flap application, all Arduino/Arduino-like modules need to be registered using the arduinoregistration.py script.
+
+**PREPARING ARDUINO**
+
+First, apply the splitflap.ino file found in arduino\splitflap\ to your arduino via the Arduino IDE. This INO file is set to look at 10 motors and sensors by default
+
+**REGISTER ARDUINO**
+
+This script assists in registering and modifying the crucial Printflapconfig.json configuration file. This file defines the size of the split-flap installation, and will require each arduino to be register in its own individual index.
+
+For example:
+
+557353231363512011C1 is registered to index position 1
+
+In a 2 x 7 grind, you will have space for 14 indexes, with index 1 being the top left, reading left to right. The script can also swap out indexes and can display the grid as defined in the config: 
+
+                ________________________________________________________________________________
+                |                                        |                                        |
+                |        1 - 5573631313835180B1B2        |        2 - 557353231363512011C1        |
+                |________________________________________|________________________________________|
+                |                                        |                                        |
+                |                                        |                                        |
+                |________________________________________|________________________________________|
+                |                                        |                                        |
+                |                                        |                                        |
+                |________________________________________|________________________________________|
+                |                                        |                                        |
+                |                                        |                                        |
+                |________________________________________|________________________________________|
+                |                                        |                                        |
+                |                                        |                                        |
+                |________________________________________|________________________________________|
+                |                                        |                                        |
+                |                                        |                                        |
+                |________________________________________|________________________________________|
+                |                                        |                                        |
+                |                                        |                                        |
+                |________________________________________|________________________________________|
+				
+
+To register an arduino, ensure that the device is plugged into the controller (as Raspberry PI or PC) and then choose a free index slot on the grid. This will allow the main.py script to know where to send each piece of text
+
+<h1>**MAIN SCRIPT**</h1>
+
+The main script usage is defined as follows:
+
+			'-m --manualinput : Manual Input Mode\n' \
+            '-s --script : Script mode, supply full path name of text file as script file\n' \
+            '-t --time : In seconds. If not using manual input, the minimum time delay between text changes\n' \
+            '-i --iterations : If using script mode, number of iterations to run the script. 0 or leave to set as indefinite\n' \
+            '-l --listener : The full directory path of the folder to listen to.  Must be supplied if running in no other mode\n' \
+            '\n' \
+            'Default mode is to run as a \'listener\' that will check every 2 seconds for the existence of a script file.\n' \
+            'This script file will be consumed by the python script'
+		
+
+There are 3 modes of operation:
+
+**Listener (default)**
+
+The script cannot run without at least a listener folder being chosen. This folder is used to listen for any files and read its text contents. This text will then be consumed by the application, removed and each line processed across the split-flap installation. This is the default option if no other argument is set.
+
+Example: python main.py -l C:\Users\Admin\Documents\GitHub\multi-split-flap\software\listener_input --- This script will look in the listener_input folder indefinitely for text files to consume, and iterate every 5 seconds
+
+**Script**
+
+This mode is defined when the full path name of a single file is chosen. This script will be read line by line, with an interval (-t) between each line in seconds. If no interval is chosen, it will default to 5 seconds.
+
+Example: python main.py -s C:\Users\Admin\Documents\GitHub\multi-split-flap\software\example_script.txt -t 2 --- This will run the script in script mode interval through each line in the file every 2 seconds indefinitely
+
+**Manual Input**
+
+This mode will initialise the modules and then wait for user input before running
+
+Example: python main.py -m
+
+**Troubleshooting**
+
+Hardware sensor output is displayed with each word iteration, including the index number of the arduino returning the fault, and the sensor number (1 - 10) returning the error:
+
+Sensor Error - Arduno no.1 - Sensor no.2 - missed_home 1 - unexpected_home 0 --- This means arduino at index 1 has a fault reported on sensor number 2
+
+At the beginning of each initialisation, all sensors will report back to the script if there is any error status:
+
+=======================================
+Arduino Index - 2 - sensor_error
+Sensor Number - 3
+=======================================
+
+This means Arduino at index 2, sensor number 3, is having a fault initialising. This would almost always be a hardware error, so hardware troubleshooting would be required until the sensor is working optimaly.
--- software/main.py
+++ software/main.py
@@ -7,32 +7,21 @@ import os
 import serial
 import serial.tools.list_ports
 import sys
-
-
-def get_text(port):
-    try:
-        flaptext = ''
-        text = port['Text']
-        result = port['Splitflap'].set_text(text)
-        for x, sensor in enumerate(result):
-            letter = '*'
-            missed_home = sensor['count_missed_home']
-            unexpected_home = sensor['count_unexpected_home']
-            if sensor['state'] == 'normal':
-                letter = sensor['flap']
-
-            if missed_home > 0 or unexpected_home > 0:
-                print('Sensor Error - Arduno no.{} - Sensor no.{} - missed_home {} - unexpected_home {}'.format(port['Index'], x + 1, missed_home, unexpected_home))
-
-            flaptext += letter
-
-        print('{} - {} - {}'.format(port['Serial Number'], port['Index'], flaptext))
-
-    except Exception as e:
-        print('Error: {}'.format(e))
+import getopt
+import time
 
 
 def run():
+
+    script_arguments = get_arguments()
+    run_mode = 'listen_mode'
+
+    if script_arguments['manual_mode']:
+        run_mode = 'manual_mode'
+    elif script_arguments['script_mode']:
+        run_mode = 'script_mode'
+
+    print('Script is running in {} '.format(run_mode))
 
     ports = get_ports()
 
@@ -63,32 +52,168 @@ def run():
     elif err_count > 0:
         print('{} have failed to initialise!'.format(err_count))
 
-    # Loop through entering text until closure. Split each string into equal pieces and send to the correct positional
-    # index.
+    # Now run with the specified mode
+
+    interval = script_arguments['time']
+
+    if run_mode == 'manual_mode':
+        run_manual_mode(ports)
+    elif run_mode == 'script_mode':
+        iterations = script_arguments['iterations']
+        script_file = script_arguments['script']
+        run_script_mode(ports, interval, script_file, iterations)
+    else:
+        folder = script_arguments['listener']
+        run_listen_mode(ports, interval, folder)
+
+
+def get_text(port):
+    try:
+        flap_text = ''
+        text = port['Text']
+        result = port['Splitflap'].set_text(text)
+        for x, sensor in enumerate(result):
+            letter = '*'
+            missed_home = sensor['count_missed_home']
+            unexpected_home = sensor['count_unexpected_home']
+            if sensor['state'] == 'normal':
+                letter = sensor['flap']
+
+            if missed_home > 0 or unexpected_home > 0:
+                print('Sensor Error - Arduno no.{} - Sensor no.{} - missed_home {} - unexpected_home {}'.format(port['Index'], x + 1, missed_home, unexpected_home))
+
+            flap_text += letter
+
+        print('{} - {} - {}'.format(port['Serial Number'], port['Index'], flap_text))
+
+    except Exception as e:
+        print('Error: {}'.format(e))
+
+
+def get_arguments():
+
+    # get command line options to determine operating mode
+    usage = '-m --manualinput : Manual Input Mode\n' \
+            '-s --script : Script mode, supply full path name of text file as script file\n' \
+            '-t --time : In seconds. If not using manual input, the minimum time delay between text changes\n' \
+            '-i --iterations : If using script mode, number of iterations to run the script. 0 or leave to set as indefinite\n' \
+            '-l --listener : The full directory path of the folder to listen to.  Must be supplied if running in no other mode\n' \
+            '\n' \
+            'Default mode is to run as a \'listener\' that will check every 2 seconds for the existence of a script file.\n' \
+            'This script will be consumed by the python script'
+
+    opts, args = getopt.getopt(sys.argv[1:], 'ms:t:i:l:', ['manualinput', 'script=', 'time=', 'iterations=', 'listener='])
+
+    command_arguments = {'time': 5,  # default time between queued up lines of text
+                         'iterations': 0,
+                         'script': None,
+                         'manual_mode': False,
+                         'script_mode': False,
+                         'listen_mode': True,
+                         'listener': None}
+
+    for opt, arg in opts:
+        if opt in ('-m', '--manualinput'):
+            command_arguments['manual_mode'] = True
+        elif opt in ('-s', '--script'):
+            command_arguments['script_mode'] = True
+            command_arguments['script'] = arg
+        elif opt in ('-i', '--iterations'):
+            command_arguments['iterations'] = int(arg)
+        elif opt in ('-t', '--time'):
+            command_arguments['time'] = int(arg)
+        elif opt in ('-l', '--listener'):
+            command_arguments['listener'] = arg
+            command_arguments['listen_mode'] = True
+
+    if len(opts) == 0:
+        print('No arguments supplied')
+        print(usage)
+        sys.exit(2)
+
+    if command_arguments['manual_mode'] and command_arguments['script_mode']:
+        print('Only one mode can be selected')
+        print(usage)
+        sys.exit(2)
+    else:
+        return command_arguments
+
+
+def run_script_mode(ports, interval, file, iterations):
+    print('Run script mode')
+    print('Time between text: {}'.format(interval))
+
+    try:
+        with open(file) as fp:
+            line_list = [line.rstrip('\n') for line in fp]  # Get file contents as list
+    except Exception as ex:
+        print('Cannot open or read file: {}'.format(file))
+        print(ex)
+        return
+
+    if iterations > 0:
+        x = 0
+        while x < iterations:
+            print('Iteration {} of {}'.format(x + 1, iterations))
+            for line in line_list:
+                set_text(ports, line)
+                time.sleep(interval)
+            x += 1
+    else:
+        while True:
+            for line in line_list:
+                set_text(ports, line)
+                time.sleep(interval)
+
+
+def run_listen_mode(ports, interval, folder):
+    print('Run listen mode')
+    print('Time between text: {}'.format(interval))
+    print('Listening for file in {} folder'.format(folder))
+
+    while True:
+        for file in os.listdir(folder):
+            print('Processing {}'.format(file))
+            try:
+                with open(os.path.join(folder, file)) as fp:
+                    line_list = [line.rstrip('\n') for line in fp]
+                    for line in line_list:
+                        set_text(ports, line)
+                        time.sleep(interval)
+            except Exception as ex:
+                print('Cannot read text in file')
+                print('{}'.format(ex))
+            finally:
+                print('Removing {}'.format(file))
+                os.remove(os.path.join(folder, file))
+
+        time.sleep(interval)
+
+
+def run_manual_mode(ports):
 
     while True:
 
         text = input("Type in a word: ")
-        text_arr = split_text(text)
-        splitflaps_update = []
+        set_text(ports, text)
 
-        for index in range(len(text_arr)):
-            x = index + 1  # increment index to account for zero based indexing
-            port = next((y for y in ports if y['Index'] == x), None)
-            if port is not None and port['Initialised']:
-                pos_index = port['Index']
-                text = text_arr[pos_index - 1].lower()  # minus 1 to account for zero based indexing
-                port['Text'] = text
-                splitflaps_update.append(port)
-                #try:
-                    #result = a_send_text(port, text)  # port['Splitflap'].set_text(text)
-                    #print(text)
-                    #print(result)
-                #except Exception as e:
-                #    print("Error {}".format(e))
 
-        pool = ThreadPool()
-        pool.map(get_text, splitflaps_update)
+def set_text(ports, text):
+
+    text_arr = split_text('{:140}'.format(text))  # Pad text to 140 blank characters if needed
+    split_flaps_update = []
+
+    for index in range(len(text_arr)):
+        x = index + 1  # increment index to account for zero based indexing
+        port = next((y for y in ports if y['Index'] == x), None)
+        if port is not None and port['Initialised']:
+            pos_index = port['Index']
+            text = text_arr[pos_index - 1].lower()  # minus 1 to account for zero based indexing
+            port['Text'] = text
+            split_flaps_update.append(port)
+
+    pool = ThreadPool()
+    pool.map(get_text, split_flaps_update)
 
 
 def split_text(x):
